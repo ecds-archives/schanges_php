1854e38a7606f7298ed7082650c18b3536ced3e3
ahickco <ahickco@bea2df84-0c34-0410-a30b-c6e753312abb>
2004-05-07 19:04:06 +0000

    modified contents.php to use taminoConnection.class

--- trunk/html/contents.php
+++ trunk/html/contents.php
@@ -2,10 +2,18 @@
 
 //one possibe way to deal with messy xqueries & the urls they produce
 
+include_once("taminoConnection.class.php");
 include("common_functions.php");
 //$url = 'http://tamino.library.emory.edu/passthru/servlet/transform/tamino/BECKCTR/ILN?_xquery=
-$url = 'http://tamino.library.emory.edu/tamino/SRC/schanges?_xquery=
-for $b in input()/TEI.2//div1
+//$url = 'http://tamino.library.emory.edu/tamino/SRC/schanges?_xquery=
+
+$args = array('host' => "vip.library.emory.edu",
+		'db' => "SRC",
+	      //	      'debug' => true,
+		'coll' => 'schanges');
+$tamino = new taminoConnection($args);
+
+$query = 'for $b in input()/TEI.2//div1
 return <div1>
  {$b/@type}
  {$b/@id}
@@ -20,7 +28,7 @@ return <div1>
    </div2>
 }</div1>
 sort by (@id)';
-$url = encode_url($url);
+//$url = encode_url($url); in taminoConnection.class
 
 $xsl_file = "contents.xsl";
 //Note: change these next lines for Schanges web pages, but parallel structure.
@@ -28,27 +36,37 @@ $xsl_file = "contents.xsl";
 
 //include("xml/head.xml");
 //include("xml/sidebar.xml");
-?>
 
-   <div class="content"> 
-          <h2>Browse</h2>
 
-<?php
+$rval = $tamino->xquery($query);
+if ($rval) {       // tamino Error code (0 = success)
+  print "<p>Error: failed to retrieve contents.<br>";
+  print "(Tamino error code $rval)</p>";
+  exit();
+} 
+
+
+print '<div class="content"> 
+          <h2>Browse</h2>'; 
 
 print "<hr>";
-   // use sablotron to transform xml
-   $xmlContent = file_get_contents($url);
+//all in taminoConnection.class   
+// use sablotron to transform xml
+   //$xmlContent = file_get_contents($url);
    //$result = transform($xmlContent, $xsl_file); 
    //print $result;
-   print $xmlContent;
+   //print $xmlContent;
+
+$tamino->xslTransform($xsl_file);
+$tamino->printResult();
+
 
 print "<hr>";
+ 
+   
+print '</div>';
+   
 
-?> 
-   
-  </div>
-   
-<?php
   //include("xml/foot.xml");
 ?>
 
