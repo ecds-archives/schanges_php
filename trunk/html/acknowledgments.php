e4174a30998731bcbdd97d212978214526969e24
ahickco <ahickco@bea2df84-0c34-0410-a30b-c6e753312abb>
2006-07-19 21:47:28 +0000

    adding files and updating site files

---
+++ trunk/html/acknowledgments.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once("config.php");
+//include_once("xmlDbConnection.class.php");
+include_once("CTI/xmlDbConnection.class.php");
+include("common_functions.php");
+
+$id = $_GET["dcidentifier"];
+
+$args = array('host' => $tamino_server,
+	      'db' => $tamino_db["meta-db"],
+	      'coll' => $tamino_coll["meta-coll"],
+	      'debug' => false);
+$tamino = new xmlDbConnection($args);
+
+html_head("Acknowledgments", true);
+
+include("xml/overview-head.xml");
+
+print '<div class="content">';
+print '<h2>Acknowledgments</h2>';
+
+//include("xml/overview.xml");
+print transform("xml/acknowledgments.xml", "xsl/overview.xsl"); 
+
+
+include("xml/footer.xml");
+
+?> 
+
+
+</body>
+</html>
