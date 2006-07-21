3f502b2eda3dfe0c17f4221de40d64c31706705c
Alice Hickcox <alice.hickcox@emory.edu>
2006-07-21 20:33:00 +0000

    this is the cti version. it works with southern changes on reagn. lets see if it works on bohr.

--- html/lib/xmlDbConnection.class.php
+++ html/lib/xmlDbConnection.class.php
@@ -1,6 +1,7 @@
 <?php 
 
 include "taminoConnection.class.php";
+include "existConnection.class.php";
 
 class xmlDbConnection {
 
@@ -21,6 +22,9 @@ class xmlDbConnection {
   var $xml;
   var $xpath;
 
+  // array of xsl & parameters to use in xsl transform
+  var $xslt;
+
   // variables for return codes/messages?
 
   // cursor variables (needed here?)
@@ -32,6 +36,8 @@ class xmlDbConnection {
   var $begin_hi;
   var $end_hi;
 
+  // for outputting debugging xml in a more helpful way
+  var $debug_count;
 
   function xmlDbConnection($argArray) {
     $this->host = $argArray['host'];
@@ -54,21 +60,38 @@ class xmlDbConnection {
     $this->xml =& $this->xmldb->xml;
     $this->xpath =& $this->xmldb->xpath;
 
+
+    // array of xsl & parameters to use in xsl transform
+    // structure: xslt[]["xsl"] = xslt filename
+    //		  xslt[]["param"] = array of parameters
+    $this->xslt = array();
+
     // variables for highlighting search terms
     // begin highlighting variables are now defined when needed, according to number of terms
     $this->end_hi = "</span>";
+
+    $this->debug_count = 0;
+
+    define("DATABASE_XML", 0);
+    define("TRANSFORMED_XML", 1);
   }
 
   // send an xquery & get xml result
   function xquery ($query, $position = NULL, $maxdisplay = NULL) {
     // pass along xquery & parameters to specified xml db
     $this->xmldb->xquery($this->encode_xquery($query), $position, $maxdisplay);
+    if ($this->debug) {
+      $this->displayXML();
+    }
   }
 
   // x-query : should only be in tamino...
   function xql ($query, $position = NULL, $maxdisplay = NULL) {
     // pass along xql & parameters to specified xml db
     $this->xmldb->xql($this->encode_xquery($query), $position, $maxdisplay);
+    if ($this->debug) {
+      $this->displayXML();
+    }
   }
 
   // retrieve cursor, total count    (xquery cursor is default)
@@ -86,8 +109,37 @@ class xmlDbConnection {
     $this->xmldb->getXqlCursor();
   }
 
+
+  // bind an xslt to parameters
+  function xslBind ($file, $params = NULL) {
+    $i = count($this->xslt);
+    $this->xslt[$i]["xsl"] = $file;
+    $this->xslt[$i]["param"] = $params;
+  }
+
+  // successively transform xml by all bound xslts, in the order that they were bound
+  function transform() {
+    $input = DATABASE_XML;	// default initial input
+    for ($i = 0; $i < count($this->xslt); $i++) {
+      $this->xslTransform($this->xslt[$i]["xsl"], $this->xslt[$i]["param"], $input);
+      $input = TRANSFORMED_XML;	// use result of last xsl transform for all subsequent inputs
+      // if debugging is turned on, display transform information & output result
+      // (particularly important for debugging more than one transform in a row)
+      if ($this->debug) {
+	print "XSLT transform with stylesheet " . $this->xslt[$i]["xsl"];
+	if (count($this->xslt[$i]["param"])) {
+	  print "<br>\nParameters: \n";
+	  foreach ($this->xslt[$i]["param"] as $key => $val) print "$key => $val \n";
+	}
+	print "<br>\n";
+	print "Result of transformation:<br>\n";
+	print $this->displayXML(TRANSFORMED_XML);
+      }
+    }
+  }
+  
    // transform the database returned xml with a specified stylesheet
-   function xslTransform ($xsl_file, $xsl_params = NULL) {
+  function xslTransform ($xsl_file, $xsl_params = NULL, $input = DATABASE_XML) {
      /* load xsl & xml as DOM documents */
      $xsl = new DomDocument();
      $xsl->load("xsl/$xsl_file");
@@ -101,31 +153,22 @@ class xmlDbConnection {
        }
      }
      /* transform the xml document and store the result */
-     $this->xsl_result = $proc->transformToDoc($this->xmldb->xml);
+     if ($input == DATABASE_XML) 	  // transform xml retrieved from the database (default)
+       $this->xsl_result = $proc->transformToDoc($this->xmldb->xml);
+     else if ($input == TRANSFORMED_XML)  // transform xml resulting from prior xsl transform
+       $this->xsl_result = $proc->transformToDoc($this->xsl_result);
    }
 
  // transform the xml created by the previous xsl transform with a specified stylesheet
    function xslTransformResult ($xsl_file, $xsl_params = NULL) {
-     /* load xsl & xml as DOM documents */
-     $xsl = new DomDocument();
-     $xsl->load("xsl/$xsl_file");
-
-     /* create processor & import stylesheet */
-     $proc = new XsltProcessor();
-     $xsl = $proc->importStylesheet($xsl);
-     if ($xsl_params) {
-       foreach ($xsl_params as $name => $val) {
-         $proc->setParameter(null, $name, $val);
-       }
-     }
-     /* transform the xsl result, and replace it with the result */
-     $this->xsl_result = $proc->transformToDoc($this->xsl_result);
+     // call default xslTransform with option to specify xml input
+     $this->xslTransform($xsl_file, $xsl_params, TRANSFORMED_XML);
    }
 
 
    function printResult ($term = NULL) {
      if ($this->xsl_result) {
-       if (isset($term[0])) {
+       if ($term[0] != NULL) {
          $this->highlightXML($term);
          // this is a bit of a hack: the <span> tags used for
          // highlighting are strings, and not structural xml; this
@@ -138,16 +181,6 @@ class xmlDbConnection {
        }
      }
 
-   }
-
-
-   // create <span> tags for highlighting based on number of terms
-   function defineHighlight ($num) {
-     $this->begin_hi = array();
-    // strings for highlighting search terms 
-    for ($i = 0; $i < $num; $i++) {
-      $this->begin_hi[$i]  = "<span class='term" . ($i + 1) . "'>";
-    }
    }
 
    // get the content of an xml node by name when the path is unknown
@@ -164,27 +197,6 @@ class xmlDbConnection {
    }
 
 
-   // Highlight the search strings within the xsl transformed result.
-   // Takes an array of terms to highlight.
-   function highlightString ($str, $term) {
-     // note: need to fix regexps: * -> \w* (any word character)
-      // FIXME: how best to deal with wild cards?
-
-     // only do highlighting if the term is defined
-     for ($i = 0; (isset($term[$i]) && ($term[$i] != '')); $i++) {
-       // replace tamino wildcard (*) with regexp -- 0 or more word characters 
-       $_term = str_replace("*", "\w*", $term[$i]);
-     // Note: regexp is constructed to avoid matching/highlighting the terms in a url 
-       $str = preg_replace("/([^=|']\b)($_term)(\b)/i",
-	      "$1" . $this->begin_hi[$i] . "$2$this->end_hi$3", $str);
-       // special case when term is at the beginning of string
-       $str = preg_replace("/(^)($_term)(\b)/i",
-        "$1" . $this->begin_hi[$i] . "$2$this->end_hi$3", $str);
-     }
-     return $str;
-   }
-
- 
    // highlight text within the xml structure
    function highlightXML ($term) {
      // if span terms are not defined, define them now
@@ -218,7 +230,7 @@ class xmlDbConnection {
 	     if (preg_match($regexp, $s)) {	// if it matches, this is one of the terms to highlight
 	       for ($i = 0; $regterm[$i] != ''; $i++) {
 	         if (preg_match("/$regterm[$i]/i", $s)) { 	// find which term it matches
-                   $newnode = $this->xsl_result->createElement("span", $s);
+                   $newnode = $this->xsl_result->createElement("span", htmlentities($s));
 	           $newnode->setAttribute("class", "term" . ($i+1));	// use term index for span class (begins at 1 instead of 0)
 	         }
 	       }
@@ -241,71 +253,111 @@ class xmlDbConnection {
      if (isset($term[0])) {
        print "<p align='center'>The following search terms have been highlighted: ";
        for ($i = 0; isset($term[$i]); $i++) {
-	 print "&nbsp; " . $this->begin_hi[$i] . "$term[$i]$this->end_hi &nbsp;";
+	 print "&nbsp; " . $this->begin_hi[$i] . htmlentities($term[$i]) . "$this->end_hi &nbsp;";
        }
        print "</p>";
      }
    }
 
- // print out links to different result sets, based on cursor
+   // create <span> tags for highlighting based on number of terms
+   function defineHighlight ($num) {
+     $this->begin_hi = array();
+    // strings for highlighting search terms 
+    for ($i = 0; $i < $num; $i++) {
+      $this->begin_hi[$i]  = "<span class='term" . ($i + 1) . "'>";
+    }
+   }
+
+   // print out links to different result sets, based on cursor
    // arguments: url to link to (pos & max # to display will be added), max
    function resultLinks ($url, $position, $max) {
      //FIXME: at least in exist, we can get a default maximum from result set itself...
      $result = "<div class='resultlink'>";
-        if ($this->count > $max) {
-          $result .= "<li class='firstresultlink'>More results:</li";
-          for ($i = 1; $i <= $this->count; $i += $max) {
-            if ($i == 1) {
-              $result .= '<li class="firstresultlink">';
-            } else { 
-              $result .= '<li class="resultlink">';
-            }
+	if ($this->count > $max) {
+	  $result .= "<li class='firstresultlink'>More results:</li";
+	  for ($i = 1; $i <= $this->count; $i += $max) {
+	    if ($i == 1) {
+	      $result .= '<li class="firstresultlink">';
+	    } else { 
+	      $result .= '<li class="resultlink">';
+	    }
             // url should be based on current search url, with new position defined
-            $myurl = $url .  "&pos=$i&max=$max";
+	    $myurl = $url .  "&pos=$i&max=$max";
             if ($i != $position) {
-              $result .= "<a href='$myurl'>";
-            }
-            $j = min($this->count, ($i + $max - 1));
-            // special case-- last set only has one result
-            if ($i == $j) {
-              $result .= "$i";
-            } else {
-              $result .= "$i - $j";
-            }
-            if ($i != $position) {
-              $result .= "</a>";
-            }
-            $result .= "</li>";
-          }
-        }
-        $result .= "</div>"; 
-        return $result;
+	      $result .= "<a href='$myurl'>";
+	    }
+    	    $j = min($this->count, ($i + $max - 1));
+    	    // special case-- last set only has one result
+    	    if ($i == $j) {
+      	      $result .= "$i";
+    	    } else {
+      	      $result .= "$i - $j";
+    	    }
+	    if ($i != $position) {
+      	      $result .= "</a>";
+    	    }
+    	    $result .= "</li>";
+	  }
+	}
+	$result .= "</div>"; 
+	return $result;
    }
 
 
   // convert a readable xquery into a clean url for tamino or exist
   function encode_xquery ($string) {
-    // get rid of multiple white spaces
-    $string = preg_replace("/\s+/", " ", $string);
-    // convert spaces to their hex equivalent
-    $string = str_replace(" ", "%20", $string);
-    // convert ampersand & # within xquery (e.g., for unicode entities) to hex
-    $string = str_replace("&", "%26", $string);
-    $string = str_replace("#", "%23", $string);
-    // convert plus sign
-    $string = str_replace("+", "%2b", $string);
-    return $string;
+    // convert multiple consecutive white spaces (of any kind) to a single space
+    // convert escaped quotes & apostrophes into their tamino entities
+    $pattern = Array("/\s+/", "/\\\'/", '/\\\"/');
+    $replace = Array(" ",     "&apos;", '&quot;');
+    $string = preg_replace($pattern, $replace, $string);
+    // convert all characters into url %## equivalent
+    return urlencode($string);
   }
 
    // print out xml (for debugging purposes)
-   function displayXML () {
-     if ($this->xml) {
-       $this->xml->formatOutput = true;
-       print "<pre>";
-       print htmlentities($this->xml->saveXML());
-       print "</pre>";
-     }
+  function displayXML ($input = DATABASE_XML) {	// by default, display xml returned from db query
+
+    if ($this->debug_count == 0) {	// first time outputting xml - also output javascript for show/hide functionality
+      print "<script language='Javascript' type='text/javascript'>
+function toggle (id) {
+  var a = document.getElementById(id); 
+  if (a.style.display == '') a.style.display = 'block';	//default 
+  a.style.display = (a.style.display != 'none') ? 'none':'block';
+}
+</script>\n";
+    }
+
+    print "<a href='javascript:toggle(\"debugxml$this->debug_count\")'>+ show/hide xml</a>\n";
+    print "<div id='debugxml$this->debug_count'>\n";
+    print "<pre>";
+    
+    switch ($input) {
+    case DATABASE_XML:
+      if ($this->xml) {
+      	$this->xml->formatOutput = true;
+	print htmlentities($this->xml->saveXML());
+      }
+      break;
+    case TRANSFORMED_XML:
+      if ($this->xsl_result) {
+	$this->xsl_result->formatOutput = true;
+	print htmlentities($this->xsl_result->saveXML());
+      }
+      break;
+    }
+    print "</pre>";
+    print "</div>";
+
+    $this->debug_count++;
    }
+
+
+  // load xml from a file (instead of getting from a query)
+  function loadxml ($file) {
+    $this->xml = new domDocument();
+    $this->xml->load($file);
+  }
 
 
 }
