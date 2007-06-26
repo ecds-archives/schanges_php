eeb3b0391874df860ebb1d79e18a957df0032fcd
rsutton <rsutton@bea2df84-0c34-0410-a30b-c6e753312abb>
2007-06-26 18:59:11 +0000

    allow file or directory as arg; split months on - or /

--- trunk/build/tag-bibl.php
+++ trunk/build/tag-bibl.php
@@ -5,47 +5,82 @@
    --splits it out into tagged sections, adds pubPlace and publisher
  */
 
-$file = $argv[1];
+$input = $argv[1];
+
+$xmlfiles = array();
+
+// if arg is a directory, process all the xml files
+if (is_dir($input)) {
+  $files = scandir($input);
+  // find all the xml files
+  foreach ($files as $f) {
+    if (substr($f, strlen($f) - 4, strlen($f)) == ".xml") {
+      array_push($xmlfiles, "$input/$f");	// use full path to file
+    }
+  }
+} elseif (is_file($input)) {
+  array_push($xmlfiles, $input);
+} else {
+  // neither file nore directory
+  print "Error: no file or directory specified.\n";
+  exit;	
+}
+
+
 
 $doc = new DOMDocument();
 $doc->resolveExternals = false;
 $doc->substituteEntities = false;
 
-$doc->load($file);
+foreach ($xmlfiles as $file) {
 
-$xpath = new DOMXpath($doc);
-$nodelist = $xpath->query("/TEI.2/teiHeader/fileDesc/sourceDesc/bibl");
-$bibl = $nodelist->item(0);
-
-$bibltxt = $bibl->nodeValue;
-// clean up whitespace to simplify subsequent regexp
-$bibltxt = preg_replace("/\s+/", ' ', $bibltxt);
-
-// split out the component parts of the bibl
-$matches = array();
-preg_match("/^(Southern\s+Changes)\.\s*(Volume.*),\s*(Number[^,.]*)[,.]\s*(.*)$/", $bibltxt, $matches);
-// first match is the entire string; subsequent matches are () sections of the regexp
-$title = $matches[1];
-$vol = $matches[2];
-$number = $matches[3];
-$date =  $matches[4];
-// get rid of any commas that might confuse strtotime, and standardize date display format
-$date = str_replace(",", "", $date);
-
-if (strpos($date, '-')) {
-  // some dates are in format month1-month2 year; use first month only for date value
-  $datestr = preg_replace("/-\w* /", " ", $date);
-} else {
-  $datestr = $date;
-}
-
-// convert text time to unix time that can be output in the proper format
-$datetime = strtotime($datestr, 0);
-
-// clear out text-only version of bibl
-$bibl->nodeValue = "";
-
-
+  $xmlparse = $doc->load($file);
+  if (! $xmlparse) {
+    print "Error: could not parse $file\n";
+    continue;
+  }
+  
+  $xpath = new DOMXpath($doc);
+  $nodelist = $xpath->query("/TEI.2/teiHeader/fileDesc/sourceDesc/bibl");
+  if ($nodelist->length != 1) {
+    // there should be one and only one match; if not, skip this file
+    print "Warning: bibl not found; skipping $file\n";
+    continue;
+  }
+  $bibl = $nodelist->item(0);
+  
+  $bibltext = $bibl->nodeValue;
+  // clean up whitespace to simplify subsequent regexp
+  $bibltext = preg_replace("/\s+/", ' ', $bibltext);
+  
+  // split out the component parts of the bibl
+  $matches = array();
+  preg_match("/^(Southern\s+Changes)\.\s*(Volume.*),\s*(Number[^,.]*)[,.]\s*(.*)$/", $bibltext, $matches);
+  // first match is the entire string; subsequent matches are () sections of the regexp
+  $title = $matches[1];
+  $vol = $matches[2];
+  $number = $matches[3];
+  $date =  $matches[4];
+  // get rid of any commas that might confuse strtotime, and standardize date display format
+  $date = str_replace(",", "", $date);
+  
+  if (strpos($date, '-')) {
+    // some dates are in format month1-month2 year; use first month only for date value
+    $datestr = preg_replace("/-\w* /", " ", $date);
+  } else if (strpos($date, '/')) {
+    // other dates are in format month1/month2 year; treat same as above
+    $datestr = preg_replace("/\/\w* /", " ", $date);
+  } else {
+    $datestr = $date;
+  }
+  
+  // convert text time to unix time that can be output in the proper format
+  $datetime = strtotime($datestr, 0);
+  
+  // clear out text-only version of bibl
+  $bibl->nodeValue = "";
+  
+  
 /* Add the content back, delimited by tags.  New format looks like this:
     <bibl>
     	<title>Southern Changes</title>.
@@ -57,26 +92,25 @@ $bibl->nodeValue = "";
     </bibl>
 */
 
-$bibl->appendChild($doc->createElement("title", $title));
-$bibl->appendChild($doc->createTextNode(". "));
-$biblscope = $bibl->appendChild($doc->createElement("biblScope", $vol));
-$biblscope->setAttribute("type", "volume");
-$bibl->appendChild($doc->createTextNode(", "));
-$biblscope = $bibl->appendChild($doc->createElement("biblScope", $number));
-$biblscope->setAttribute("type", "issue");
-$bibl->appendChild($doc->createTextNode(". "));
-$datenode = $bibl->appendChild($doc->createElement("date", "$date"));
-$datenode->setAttribute("value", date("Y-m", $datetime));
-$bibl->appendChild($doc->createTextNode(". "));
-$bibl->appendChild($doc->createElement("pubPlace", "Atlanta, GA"));
-$bibl->appendChild($doc->createTextNode(": "));
-$bibl->appendChild($doc->createElement("publisher", "Southern Regional Council"));
-$bibl->appendChild($doc->createTextNode("."));
-
-
-/* when we're happy with the new output, save back to a file-- original or a new one? */
-$tmpfile = "/tmp/schanges.xml";
-print "saving output to $tmpfile\n";
-$doc->save($tmpfile);
+  $bibl->appendChild($doc->createElement("title", $title));
+  $bibl->appendChild($doc->createTextNode(". "));
+  $biblscope = $bibl->appendChild($doc->createElement("biblScope", $vol));
+  $biblscope->setAttribute("type", "volume");
+  $bibl->appendChild($doc->createTextNode(", "));
+  $biblscope = $bibl->appendChild($doc->createElement("biblScope", $number));
+  $biblscope->setAttribute("type", "issue");
+  $bibl->appendChild($doc->createTextNode(". "));
+  $datenode = $bibl->appendChild($doc->createElement("date", "$date"));
+  $datenode->setAttribute("value", date("Y-m", $datetime));
+  $bibl->appendChild($doc->createTextNode(". "));
+  $bibl->appendChild($doc->createElement("pubPlace", "Atlanta, GA"));
+  $bibl->appendChild($doc->createTextNode(": "));
+  $bibl->appendChild($doc->createElement("publisher", "Southern Regional Council"));
+  $bibl->appendChild($doc->createTextNode("."));
+  
+  
+  /* when we're happy with the new output, save back to a file-- original or a new one? */
+  $doc->save($file);
+}
 
 ?>
