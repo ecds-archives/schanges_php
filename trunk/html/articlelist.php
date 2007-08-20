0708fb2f9dd0c2ae1c5e1085b4b7f18c4d1b9edd
ahickco <ahickco@bea2df84-0c34-0410-a30b-c6e753312abb>
2007-08-20 15:16:37 +0000

    updated xquery

--- trunk/html/articlelist.php
+++ trunk/html/articlelist.php
@@ -6,7 +6,7 @@ include_once("lib/xmlDbConnection.class.php");
 include("common_functions.php");
 
 $id = $_GET["id"];
-$docdate = $_GET["docdate"];
+//$docdate = $_GET["docdate"];
 
 $exist_args{"debug"} = false;
 $xmldb = new xmlDbConnection($exist_args);
@@ -21,34 +21,27 @@ print '<div class="content">';
 print '<h2>Articles</h2>';
 
 //query for single issue list of articles
-$query = '<result>
-{for $b in /TEI.2//div1[@id = "' . "$id" . '"]
-return 
-<issue-id>{$b/@id}
-{$b/head}</issue-id>
-}
-{let $curdate := ("' . "$docdate" . '")
+$query = 'for $issue in /TEI.2//div1[@id = "' . "$id" . '"]
+let $curdate := $issue/p/date/@value
 let $previd := (for $d in /TEI.2//div1[p/date/@value < $curdate]
-    order by $d/p/date/@value
-return $d)[last()]
-return 
+    order by $d/p/date/@value return $d)[last()]
+let $nextid := (for $c in /TEI.2//div1[p/date/@value > $curdate]
+    order by $c/p/date/@value return $c)[1]
+
+return <result>
+<issue-id>{$issue/@id}
+{$issue/head}</issue-id>
     <prev>
     {$previd/@id}
      <docdate>{$previd/p/date/@value}</docdate>
     {$previd/head}
 </prev>
-}
-x{let $curdate := ("' . "$docdate" . '")
-let $nextid := (for $c in /TEI.2//div1[p/date/@value > $curdate]
-    order by $c/p/date/@value
-return $c)[1]
-return
 <next>
 {$nextid/@id}
  <docdate>{$nextid/p/date/@value}</docdate>
   {$nextid/head}
-</next>}
-{for $a in /TEI.2//div1[@id = "' . "$id" . '"]/div2
+</next>
+{for $a in $issue/div2
 	  order by xs:int($a/@n)
 return
 <article>
@@ -59,7 +52,9 @@ return
 {$a/docDate}
 </article>
 }
-</result>';
+
+</result>
+';
 
 $xsl_file = "article-list.xsl";
 
