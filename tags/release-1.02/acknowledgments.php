8221b86bf8ab19fdc80b91e07ffae4537c168912
ahickco <ahickco@bea2df84-0c34-0410-a30b-c6e753312abb>
2010-03-12 18:53:21 +0000

    updated oai query xsl

---
+++ tags/release-1.02/acknowledgments.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once("config.php");
+//include_once("xmlDbConnection.class.php");
+include_once("lib/xmlDbConnection.class.php");
+include("common_functions.php");
+
+
+html_head("Acknowledgments", true);
+print "</head>";
+include("web/xml/overview-head.xml");
+
+print '<div class="content">';
+print '<h2>Acknowledgments</h2>';
+
+
+print transform("web/xml/acknowledgments.xml", "xslt/overview.xsl"); 
+
+
+include("web/xml/footer.xml");
+
+?> 
+
+
+</body>
+</html>
